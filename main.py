8dda513e8551186f75eade49586dbe75d5630523
Damon18184050 <damondeardeng@gmail.com>
2026-04-28 00:36:18 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from telegram import Update
-from telegram.ext import ApplicationBuilder, MessageHandler, filters, ContextTypes
+from telegram.ext import ApplicationBuilder, MessageHandler, ContextTypes, filters
 
-TOKEN = "8604959641:AAFLVWtV9BdB4Qhreu5pqEOMgaWvyV41T2E"
-SOURCE_CHAT_ID = -1003983646730
-TARGET_CHAT_ID = -1001174798090
+TOKEN = "你的TOKEN"
+SOURCE_CHAT_ID = -100xxxxxxxxxx
+TARGET_CHAT_ID = -100xxxxxxxxxx
+
 
 async def forward(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if update.channel_post and update.channel_post.chat.id == SOURCE_CHAT_ID:
@@ -13,7 +14,9 @@ async def forward(update: Update, context: ContextTypes.DEFAULT_TYPE):
             message_id=update.channel_post.message_id
         )
 
+
 app = ApplicationBuilder().token(TOKEN).build()
+
 app.add_handler(MessageHandler(filters.ALL, forward))
 
 app.run_polling()
